07f729e1089f059fe23de1850ba3104dc794b411
doyan-dev <db9326300292@gmail.com>
2026-08-19 06:54:35 +0530

    implemented user table

--- src/app.js
+++ src/app.js
@@ -1,4 +1,5 @@
 import express from "express"
+import { User } from "./models/userModel.js";
 import dbconnection from "./config/db.js"
 import dotenv from "dotenv"
 dotenv.config();
@@ -6,7 +7,21 @@ const app = express();
 
 const port =process.env.PORT
 
+//middlewear
+app.use(express.json())
 
+// //testing 
+// app.post("/",async(req,res)=>{
+//     const {username,password,bio}=req.body;
+//  const user=await User.create({
+//     username,
+//     password,
+//     bio
+// });
+// console.log(user);
+// res.json({message:"syccess"})
+
+// })
 
 app.listen(`${port}`,()=>{
     console.log(`server connected  with port ${port}`);
---
+++ src/models/userModel.js
@@ -0,0 +1,31 @@
+import mongoose from "mongoose";
+import dbconnection from "../config/db.js";
+
+const userSchema =new mongoose.Schema( {
+    username:{
+        required:true,
+        type:String,
+        maxlength:20
+    },
+
+    password:{
+        required:true,
+        type:String,
+        
+    },
+
+    bio:{
+        required:true,
+        type:String,
+        maxlength :300
+    },
+    
+    
+},
+{
+        timestamps:true
+    }
+)
+
+
+export let User = mongoose.model("User",userSchema);
