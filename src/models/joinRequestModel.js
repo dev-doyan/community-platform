1f4e2f9cfbf73e3829459c4cc2391cae7020b694
doyan-dev <db9326300292@gmail.com>
2026-08-20 06:16:56 +0530

    .

---
+++ src/models/joinRequestModel.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const joinRequestSchema = new mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    community: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Community",
+        required: true
+    },
+
+    requestedAt: {
+        type: Date,
+        default: Date.now
+    }
+});
+
+joinRequestSchema.index(
+    { user: 1, community: 1 },
+    { unique: true }
+);
+
+export const JoinRequest = mongoose.model(
+    "JoinRequest",joinRequestSchema
+);
